75541e1ea4db3e448a40e2dda6241d2ccdb84e3f
Candidate Kristi <aaron+kristi@superformula.com>
2019-09-12 10:50:06 -0400

    Setup/style config/#4 (#7)
    * styles applied to app, storybook
    * better organized styles, storybook polish

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import TestComponent from "../components/TestComponent";
+
 export interface HelloProps {
   compiler: string;
   framework: string;
@@ -8,5 +10,6 @@ export interface HelloProps {
 export const Home = (props: HelloProps) => (
   <h1>
     Hello from {props.compiler} and {props.framework}!
+    <TestComponent title={props.compiler} />
   </h1>
 );
